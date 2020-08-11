4339890cbf23baa3bc0ca60b06db3f763d5ddabb
OrMizrahi <51017125+OrMizrahi@users.noreply.github.com>
2020-08-11 22:39:11 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,13 @@
+const path = require ('path');
+
 module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
+  outputDir:path.resolve(__dirname,'./server/public'),
   devServer: {
     proxy: 'http://localhost:3000/',
   }
 }
+
 
